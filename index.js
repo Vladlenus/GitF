af9442a9ccccff9454d7be87be0d1ba285afa4d6
Vladlenus <110651671+Vladlenus@users.noreply.github.com>
2022-08-27 20:25:31 +0300

    First commit
    First commit

---
+++ index.js
@@ -0,0 +1,58 @@
+
+import path from'path';
+
+import express from 'express';
+import mongoose from 'mongoose';    
+import UserModel from './models/User.js';
+
+
+const createPath = (page) => path.resolve( `${page}.html`);
+
+mongoose.connect('mongodb+srv://valdis:1234@cluster0.lrnns58.mongodb.net/blog?retryWrites=true&w=majority')
+
+.then(()=> console.log('ss galiciya end'))
+.catch(() => console.log('pomylka'))
+
+const app = express(); 
+
+console.log('1 nane')
+app.use(express.json())
+
+app.post('/auth/register', async(req, res)=>{res.send('111jello'), console.log('ssssss')
+const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    
+});
+
+console.log(doc)
+
+    const user = await doc.save();
+})
+
+app.get('/auth/register',(req, res)=>{
+    res.sendFile(createPath('index'));
+});
+console.log('2 sex')
+
+app.listen(4444, (err)=> {
+    if(err){
+        return console.log('sos')
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
