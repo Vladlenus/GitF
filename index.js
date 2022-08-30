c8c4d88f117ecd59f389e48906921b35f6fed11f
Vladlenus <110651671+Vladlenus@users.noreply.github.com>
2022-08-31 02:43:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
-console.log('2 sex')
-import path from'path';
-
-import express from 'express';
-import mongoose from 'mongoose';    
-import UserModel from './models/User.js';
-
-
-const createPath = (page) => path.resolve( `${page}.html`);
-
-mongoose.connect('mongodb+srv://valdis:1234@cluster0.lrnns58.mongodb.net/blog?retryWrites=true&w=majority')
-
-.then(()=> console.log('ss galiciya end'))
-.catch(() => console.log('pomylka'))
-
-const app = express(); 
-
-console.log('1 nane')
-app.use(express.json())
-
-app.post('/auth/register', async(req, res)=>{res.send('111jello'), console.log('ssssss')
-const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
+`<li>
+    ${content[key].name}<h1>${content[key].name}</h1>
+    <img src="${content[key].text}"   alt="${content[key].text}">
+    </li>
     
-});
+    `
+// console.log('2 sex')
+// import path from'path';
 
-// console.log(doc)
+// import express from 'express';
+// import mongoose from 'mongoose';    
+// import UserModel from './models/User.js';
 
-    const user = await doc.save();
-})
 
-app.get('/auth/register',(req, res)=>{
-    res.sendFile(createPath('index'));
-});
-console.log('2 sex')
+// const createPath = (page) => path.resolve( `${page}.html`);
 
-app.listen(process.env.PORT ||4444, (err)=> {
-    if(err){
-        return console.log('sos')
-    }
-});
+// mongoose.connect('mongodb+srv://valdis:1234@cluster0.lrnns58.mongodb.net/blog?retryWrites=true&w=majority')
+
+// .then(()=> console.log('ss galiciya end'))
+// .catch(() => console.log('pomylka'))
+
+// const app = express(); 
+
+// console.log('1 nane')
+// app.use(express.json())
+
+// app.post('/auth/register', async(req, res)=>{res.send('111jello'), console.log('ssssss')
+// const doc = new UserModel({
+//     email: req.body.email,
+//     fullName: req.body.fullName,
+    
+// });
+
+// // console.log(doc)
+
+//     const user = await doc.save();
+// })
+
+// app.get('/auth/register',(req, res)=>{
+//     res.sendFile(createPath('index'));
+// });
+// console.log('2 sex')
+
+// app.listen(process.env.PORT ||4444, (err)=> {
+//     if(err){
+//         return console.log('sos')
+//     }
+// });
 
 
 
